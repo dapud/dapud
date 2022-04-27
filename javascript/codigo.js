844962beffcf300ebea8980a16632effd6caed05
davidapud <dapud@hotmail.com.ar>
2022-04-26 23:53:11 -0300

    entrega final 260422

--- javascript/codigo.js
+++ javascript/codigo.js
@@ -1,5 +1,5 @@
-class oferta {
-  constructor(tipo, destino, dias, precio, pension) {
+   class oferta {
+   constructor(tipo, destino, dias, precio, pension) {
     this.tipo = tipo;
     this.destino = destino;
     this.dias = dias;
@@ -19,94 +19,104 @@ class oferta {
       this.pension +
       " y el valor por persona es de $" +
       this.precio +
-      ".-"
-    );
-  }
-}
+        ".-"
+      );
+      }
+     }
 
-const oferta1 = new oferta("aventura", "mendoza", 10, 20000, "media pension ");
-const oferta2 = new oferta("relax", "san juan ", 15, 30000, "pension completa");
-const oferta3 = new oferta("romantica", "tucuman", 17, 40000, "media pension");
-const oferta4 = new oferta(
-  "diversion",
-  "san carlos de bariloche",
-  25,
-  60000,
-  "pension completa"
-);
-const oferta5 = new oferta("cultura", "Salta", 5, 5000, "media pension");
-const oferta6 = new oferta(
-  "relax",
-  "San Miguel de Tucuman",
-  5,
-  5000,
-  "media pension"
-);
-const oferta7 = new oferta("romantica", "Arequito", 5, 5000, "media pension");
-const oferta8 = new oferta("diversion", "El Naranjo", 5, 5000, "media pension");
-const oferta9 = new oferta("acentura", "Canals", 5, 5000, "media pension");
-const oferta10 = new oferta("aventura", "Cordoba", 5, 5000, "media pension");
+      const oferta1 = new oferta("aventura", "mendoza", 10, 20000, "media pension ");
+      const oferta2 = new oferta("relax", "san juan ", 15, 30000, "pension completa");
+      const oferta3 = new oferta("romantico", "tucuman", 17, 40000, "media pension");
+      const oferta4 = new oferta("diversion","san carlos de bariloche",25,60000,"pension completa");
+      const oferta5 = new oferta("cultura", "Salta", 5, 5000, "media pension");
+      const oferta6 = new oferta("relax","San Miguel de Tucuman",5,5000,"media pension");
+      const oferta7 = new oferta("romantica", "Arequito", 5, 5000, "media pension");
+      const oferta8 = new oferta("diversion", "El Naranjo", 5, 5000, "media pension");
+      const oferta9 = new oferta("acentura", "Canals", 5, 5000, "media pension");
+      const oferta10 = new oferta("aventura", "Cordoba", 5, 5000, "media pension");
 
-function getSelectValue() {
+
+
+
+ function getSelectValue() {
   var selectedValue = document.getElementById("opAven1").value;
   console.log(selectedValue);
 
   switch (selectedValue) {
     case "1": 
-      alert(
-        "Perfecto, elegiste el mejor viaje de Aventura, a continuacion te cuento los detalles."
-      );
 
       if (selectedValue === "1") { 
         oferta1.mensaje();
         break;
       }
       case "2":
-        alert(
-          "Perfecto, elegiste el mejor viaje de Relax, a continuacion te cuento los detalles."
-        );
-
+       
         if (selectedValue === "2") {
           oferta2.mensaje();
           break;
         }
         case "3":
-          alert(
-            "Perfecto, elegiste el mejor Viaje Romentico, a continuacion te cuento los detalles."
-          );
-
+          
           if (selectedValue === "3") {
             oferta3.mensaje();
             break;
           }
           case "4":
-            alert(
-              "Perfecto, elegiste divertirte a full, a continuacion te cuento los detalles."
-            );
-
+           
             if (selectedValue === "4") {
               oferta4.mensaje();
               break;
             }
             case "5":
-              alert(
-                "Perfecto, elegiste el mejor viaje Cultural, a continuacion te cuento los detalles."
-              );
-
+             
               if (selectedValue === "5") {
                 oferta5.mensaje();
                 break;
-              }
+              }  ;
 
-              default:
-                alert(
-                  "Creo que no entendi lo que escribiste, podrias verificar que el nuemro de opcion sea la correcta?"
-                );
+                   
+              
   }
+  mensaje()  
+}
 
-  alert(
-    "A continuacion te detallamos todos los destinos nacionales a los que podes acceder"
-  );
+  
+
+    function mensajeDestinos(){
+    
+    swal(destinoNacional.join("\n"));
+  
+    }
+    function masDestinos(){
+    
+      swal(destinoCultural.join("\n"));
+         
+      }
+      const destinoCultural = [
+        "Córdoba ",
+        "Paraná ",
+        "Formosa ",
+        "San Salvador de Jujuy ",
+        
+        "La Rioja ",
+        "Mendoza ",
+        "Posadas ",
+        "Neuquén ",
+        "Viedma ",
+        "Salta ",
+        
+        "San Luis ",
+        "Río Gallegos ",
+        "Santa Fe ",
+        
+        "Ushuaia ",
+        
+      ];
+
+
+
+  
+
   const destinoNacional = [
     "San Fernando del Valle de Catamarca ",
     "Resistencia ",
@@ -132,11 +142,11 @@ function getSelectValue() {
     "San Miguel de Tucumán ",
   ];
 
-  alert(destinoNacional.join("\n"));
-  alert(
-    "te lo ordeno alfabeticamente para que sea mas facil encontrar tu destino"
-  );
-  alert(destinoNacional.sort());
+
+
+
+
+  
 
   const nuevosDestinos = document.getElementById("nuevosDestinos");
   for (let i = 0; i < destinoNacional.length; i++) {
@@ -145,7 +155,9 @@ function getSelectValue() {
     nuevosDestinos.appendChild(li);
     console.log(nuevosDestinos);
   }
-}
+
+
+
 const tituloPrincipal = document.getElementById("tituloPrincipal");
 
 tituloPrincipal.innerText = " Viajamos a donde quieras !!!";
@@ -186,12 +198,7 @@ function guardarEnLocalStorage() {
 }
 
 
-//me llevo el set de datos almacenados en la constante JSON, a un archivo .json independiente
-//Tendré que agregarle las comillas a cada propiedad, porque el archivo .json solo entiende strings
-//Resuelto esto, ahora tendré que recurrir a fetch() para leer su contenido e integrarlo en JS
-//los archivos .JSON no soportan código JS en su interior. Tenemos que poner una estructura íntegramente JSON
-//no variables, no constantes, no condicionales, nada que tenga que ver con JS
-//el archivo .JSON simulará un backend que no tenemos, y nos proveerá esa información que nuestra app web necesita
+
 
 const inputFiltrar = document.querySelector("#filtrar")
 const tbody = document.querySelector("tbody")
@@ -235,3 +242,33 @@ const cargoGrillaOfertas = ()=> {
 
 document.addEventListener("DOMContentLoaded", obtenerDatos)
   
+
+
+function btnRomantico(){
+ 
+  swal("Quieres conocer a personas que no quieren viajar solas?", {
+    buttons: {
+      cancel: " Noo, estoy bien solo!",
+      catch: {
+        text: "Si, no me gusta viajar solo!",
+        value: "desopilante",
+      }
+     
+    }
+  })
+  .then((value) => {
+    switch (value) {
+   
+      case "desopilante":
+        swal("Perfecto!", "una gran aventura los espera", "success");
+        break;
+   
+      default:
+        swal(" Creo que tomaste la mejor decision , estan todos locos jajaja!");
+    }
+  });
+  
+};
+  
+ 
+ 
